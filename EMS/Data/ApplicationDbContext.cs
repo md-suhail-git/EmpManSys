acd1cc4342d6e788487aa94e84786ce5975750a9
md-suhail-git <mdsuhail1502@gmail.com>
2024-11-30 23:48:49 +0530

    Add project files.

---
+++ EMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EMS.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }   
+    }
+}
---
+++ EMS/Models/UserActivity.cs
@@ -0,0 +1,13 @@
+﻿using System.Security;
+
+namespace EMS.Models
+{
+    public class UserActivity
+    {
+        public String? CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public String? ModifiedById { get; set; }
+        public DateTime ModifiedOn { get; set; }
+
+    }
+}
